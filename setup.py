42433331fe0389e848c0ed42cebef1c9ae56df78
rdbende <rdbende@gmail.com>
2021-06-25 15:27:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name="python-tkdnd",
-    version="0.1",
+    version="0.2",
     description="Native drag & drop capabilities in tkinter. tkinterDnD is a nice wrapper around the tkdnd tcl package.",
     author="rdbende",
     author_email="rdbende@gmail.com",
     url="https://github.com/rdbende/tkinterDnD",
     install_requires=['ttkwidgets>=0.11.0'],
     python_requires='>=3.6',
+    license="MIT license",
     packages=["tkinterDnD"],
     package_data={"tkinterDnD": ["windows/*", "linux/*", "mac/*"]}
 )
